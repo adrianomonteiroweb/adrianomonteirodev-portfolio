3b27a2007146490137947afe473de0bbb72dc879
adriano <adrianomonteiroweb@gmail.com>
2022-09-23 14:37:52 -0300

    feat: cypress e2e test to elements visible in the viewports and languages menu

--- packages/client/cypress/e2e/profile-screen.spec.cy.ts
+++ packages/client/cypress/e2e/profile-screen.spec.cy.ts
@@ -6,12 +6,30 @@ const REACT_SERVER =
     : process.env.REACT_APP_DEPLOY_URI;
 
 describe('E2E tests on the profile screen.', () => {
-  it('The following elements must exist.', () => {
+  it('The following elements should be visible with the 390x844 viewport.', () => {
     cy.viewport(390, 844);
 
     cy.visit(`${REACT_SERVER}`);
 
+    cy.get('[data-cy="menu-languages"]');
+
     cy.get('[data-cy="menu"]');
+
+    cy.get('[data-cy="welcome-text"]');
+
+    cy.get('[data-cy="about-me-text"]');
+
+    cy.get('[data-cy="profile-image"]');
+  });
+
+  it('The following elements should be visible with the 1444x844 viewport.', () => {
+    cy.viewport(1444, 844);
+
+    cy.visit(`${REACT_SERVER}`);
+
+    cy.get('[data-cy="menu-languages"]');
+
+    cy.get('[data-cy="menu"]').should('not.be.visible');
 
     cy.get('[data-cy="welcome-text"]');
 
--- packages/client/src/data/images.ts
+++ packages/client/src/data/images.ts
@@ -1,3 +1,8 @@
 import me from './images/me.png';
 
-export { me };
+import brazilianFlag from './images/brazil.png';
+import unitStatesFlag from './images/united-states.png';
+import spainFlag from './images/spain.png';
+import koreanFlag from './images/south-korea.png';
+
+export { me, brazilianFlag, unitStatesFlag, spainFlag, koreanFlag };
