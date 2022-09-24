b7bda002ce324cf8f9228f35b73481a551342a30
adriano <adrianomonteiroweb@gmail.com>
2022-09-24 10:12:21 -0300

    feat: implement translation with i18next

--- packages/client/src/i18next/index.ts
+++ packages/client/src/i18next/index.ts
@@ -3,20 +3,28 @@ import { initReactI18next } from 'react-i18next';
 
 import PTBR from './locales/pt-br/pt-br.json';
 import ENUS from './locales/en-us/en-us.json';
+import ESES from './locales/es-es/es-es.json';
+import KOKR from './locales/ko-kr/ko-kr.json';
 
 const resources = {
   'pt-BR': PTBR,
   'en-US': ENUS,
+  'es-ES': ESES,
+  'ko-KR': KOKR,
 };
 
-i18next.use(initReactI18next).init({
-  resources,
-  // lng para pegar da navegação a linguagem
-  lng: navigator.language,
-  interpolation: {
-    // interpolation para evitar injeção xls
-    escapeValue: false,
-  },
-});
+function InitI18next() {
+  i18next.use(initReactI18next).init({
+    resources,
+    // lng para pegar da navegação a linguagem
+    lng: navigator.language,
+    interpolation: {
+      // interpolation para evitar injeção xls
+      escapeValue: false,
+    },
+  });
 
-export default i18next;
+  return i18next;
+}
+
+export default InitI18next();
