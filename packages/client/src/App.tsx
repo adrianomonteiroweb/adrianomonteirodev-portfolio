9719cddf4ec32d9af1d30150acd86daa6f7af06a
adriano <adrianomonteiroweb@gmail.com>
2022-09-25 18:59:31 -0300

    feat: implement mult theme using styled components

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import './App.css';
 
@@ -6,22 +7,44 @@ import MenuComponent from './components/menu/MenuComponent';
 import MenuLanguages from './components/menu-languages/MenuLanguages';
 
 import ProfileScreen from './pages/profile-screen/ProfileScreen';
+import { useState } from 'react';
+import { darkTheme, lightTheme } from './styled-components/themes';
+import { Container, Content } from './styled-components/styledComponents';
+import { GlobalStyles } from './styled-components/GlobalStyles';
+import { useTranslation } from 'react-i18next';
 
 export default function App() {
+  const { t } = useTranslation();
+  const [theme, setTheme] = useState('Light');
+
+  function changeTheme() {
+    theme === 'Light' ? setTheme('Dark') : setTheme('Light');
+  }
+
   return (
-    <main>
-      {process.env.REACT_APP_DEV === 'true' && (
-        <span>Em desenvoldimento...</span>
-      )}
-      <Router>
-        <MenuLanguages />
-        <section className='content'>
-          <MenuComponent />
-          <Routes>
-            <Route path='/' element={<ProfileScreen />} />
-          </Routes>
-        </section>
-      </Router>
-    </main>
+    <ThemeProvider theme={theme === 'Light' ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <Container>
+        <main>
+          {process.env.REACT_APP_DEV === 'true' && (
+            <span>Em desenvoldimento...</span>
+          )}
+          <Router>
+            <MenuLanguages />
+            <Content className='content'>
+              {
+                <button className='theme-button' onClick={() => changeTheme()}>
+                  {t(`${theme} Theme`)}
+                </button>
+              }
+              <MenuComponent />
+              <Routes>
+                <Route path='/' element={<ProfileScreen />} />
+              </Routes>
+            </Content>
+          </Router>
+        </main>
+      </Container>
+    </ThemeProvider>
   );
 }
