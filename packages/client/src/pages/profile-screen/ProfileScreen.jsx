e5b9d2bc128eb64757c9655ed16fd99fe0696310
adriano <adrianomonteiroweb@gmail.com>
2022-09-27 22:00:51 -0300

    feat: style css to terminal text

--- packages/client/src/pages/profile-screen/ProfileScreen.jsx
+++ packages/client/src/pages/profile-screen/ProfileScreen.jsx
@@ -5,32 +5,14 @@ import { me } from '../../data/images';
 import './profileScreen.css';
 
 export default function ProfileScreen() {
-  const { t } = useTranslation();
   return (
-    <div className='profile-screen-div'>
-      <div className='profile-div'>
-        <div className='about-me-div'>
-          <h2 data-cy='welcome-text' className='welcome-text'>
-            {t('Hey there. I`m Adriano Monteiro.')}
-          </h2>
-          <p>{t('Junior Full Stack Software Developer.')}</p>
-        </div>
-        <div className='profile-image-div'>
-          <img
-            data-cy='profile-image'
-            className='profile-image'
-            src={me}
-            alt='Adriano Monteiro'
-          />
-        </div>
-      </div>
-      <div className='text-div'>
-        <p data-cy='about-me-text' className='about-me-text'>
-          {t(
-            'A software developer passionate about learning, teaching and collaborating. How can I contribute to your team?'
-          )}
-        </p>
-      </div>
+    <div className='terminal-div'>
+      <p className='text-terminal'>
+        <span className='root'>adriano</span>@
+        <span className='user'>software-developer</span> /home{' '}
+        <span className='hashtag'>#</span>
+        <span className='cursor'>|</span>
+      </p>
     </div>
   );
 }
